855c892a600095a97bad4e569c0ac29721a32ccc
Volodymyr Kulivatskyy <kulivackijvoloda@gmail.com>
2024-11-12 13:14:18 +0200

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { Forth } from "./components/Forth";
 import { Fiveth } from "./components/Fiveth";
 import { Six } from "./components/Six";
 import { Seven } from "./components/Seven";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
@@ -33,35 +34,7 @@ function App() {
         <Seven />
       </main>
 
-      <footer>
-        <div className="footer_logo">
-          <img src="" alt="logo" />
-          <img src="" alt="logo_text" />
-        </div>
-        <ul>
-          <li>PRODUCT</li>
-          <li>REVIEWS</li>
-          <li>VIDEO</li>
-          <li>DESCRIPTION</li>
-          <li>ADDITIONAL PRODUCTS</li>
-        </ul>
-        <ul>
-          <li>SHIPPING</li>
-          <li>PAYMENT</li>
-          <li>HOW TO ORDER</li>
-          <li>USER AGREEMENT</li>
-          <li>ADDITIONAL PRODUCTS</li>
-          <li>RETURN</li>
-        </ul>
-        <div className="footer_block">
-          <span>WE ARE IN SOCIAL NETWORKS:</span>
-          <ul>
-            <img src="" alt="instagram" />
-            <img src="" alt="facebook" />
-            <img src="" alt="youtube" />
-          </ul>
-        </div>
-      </footer>
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,46 @@
+import "../styles/main.scss"
+import "../styles/index.scss"
+import logo from "../assets/logo.svg"
+import logo_text from "../assets/logo_text.svg"
+import insta from "../assets/instagram.svg"
+import face from "../assets/facebook.svg"
+import youtube from "../assets/youtube.svg"
+
+export const Footer = () => {
+    return (
+        <footer>
+            <div className="footer_wrap">
+                <div className="footer_logo">
+                    <img src={logo} alt="logo" />
+                    <img src={logo_text} alt="logo_text" />
+                </div>
+                <ul>
+                    <li>PRODUCT</li>
+                    <li>REVIEWS</li>
+                    <li>VIDEO</li>
+                    <li>DESCRIPTION</li>
+                    <li>ADDITIONAL PRODUCTS</li>
+                </ul>
+                <ul>
+                    <li>SHIPPING</li>
+                    <li>PAYMENT</li>
+                    <li>HOW TO ORDER</li>
+                    <li>USER AGREEMENT</li>
+                    <li>ADDITIONAL PRODUCTS</li>
+                    <li>RETURN</li>
+                </ul>
+                <div className="footer_block">
+                    <p>WE ARE IN SOCIAL NETWORKS:</p>
+                    <ul>
+                        <li><img src={insta} alt="instagram" /></li>
+                        <li><img src={face} alt="facebook" /></li>
+                        <li><img src={youtube} alt="youtube" /></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="footer_copyright">
+                <p>2023 All right reserved. Development and improvement of QuatroIT</p>
+            </div>
+        </footer>
+    )
+}
