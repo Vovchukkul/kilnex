5d028216dd06f3b30f6705cf93f1669fecf76055
Volodymyr Kulivatskyy <kulivackijvoloda@gmail.com>
2024-11-21 19:05:19 +0200

    add

--- src/components/ShopButton.tsx
+++ src/components/ShopButton.tsx
@@ -1,11 +1,13 @@
-// import button from '../assets/button2.svg';
+import button from '../assets/shop_btn_bot.svg';
 import '../styles/index.scss';
 import '../styles/main.scss';
 
 export const ShopButton = () => {
     return (
-        <button className='adv_btn'>
-            <span>SHOP NOW</span>
-        </button>
+        <div className="wrap">
+            <button className='adv_btn'>
+                <span>SHOP NOW</span>
+            </button>
+        </div>
     )
 }
