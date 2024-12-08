0cbcd0d6b14289e921c8fddd17e55e2e46f80c64
Volodymyr Kulivatskyy <kulivackijvoloda@gmail.com>
2024-12-08 16:41:34 +0200

    adjusted header and aside for all

---
+++ src/components/Burger.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import styles from "./Burger.module.scss";
+
+type Props = {
+    isOpen: boolean,
+    click: () => void
+}
+
+export const Burger: React.FC<Props> = ({ isOpen, click }) => {
+//   const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    click()
+  };
+
+  return (
+    <div
+      id="nav-icon1"
+      className={`${styles.navIcon} ${isOpen ? styles.open : ""}`}
+      onClick={toggleMenu}
+    >
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+};
